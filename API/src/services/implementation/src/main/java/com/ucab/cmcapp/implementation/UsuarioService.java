28d8042601eeb78cd0d1aaf18ecc1b0b5161b0a8
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-11-09 14:36:59 -0400

    Modificando rutas de usuario
    se ha cambiando los nombres de las rutas de usuarioService

--- API/src/services/implementation/src/main/java/com/ucab/cmcapp/implementation/UsuarioService.java
+++ API/src/services/implementation/src/main/java/com/ucab/cmcapp/implementation/UsuarioService.java
@@ -19,7 +19,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/users")
+@Path("/usuario")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UsuarioService extends BaseService {
