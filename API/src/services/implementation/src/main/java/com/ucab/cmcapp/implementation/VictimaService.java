5bf81a5b67e0b5c7e4d51be763a9608160a8107c
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-12-03 09:37:49 -0400

    Cambios de mensaje en victima
    Se ha cambiado los mensajes de exito/error personalizados en el servicio de victima (endpoints)

--- API/src/services/implementation/src/main/java/com/ucab/cmcapp/implementation/VictimaService.java
+++ API/src/services/implementation/src/main/java/com/ucab/cmcapp/implementation/VictimaService.java
@@ -35,7 +35,7 @@ public class VictimaService extends BaseService {
             if (command.getReturnParam() != null)
                 responseDTO = Usuario_VictimaMapper.mapEntityToDto(command.getReturnParam());
             else
-                return Response.status(Response.Status.OK).entity(new CustomResponse<>("El ID " + victimaId + " del historico no existe en la BBDD ")).build();
+                return Response.status(Response.Status.OK).entity(new CustomResponse<>("El ID " + victimaId + " de la victima no existe en la BBDD ")).build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>(e, "Error interno en la ruta ID " + e.getMessage())).build();
         } finally {
@@ -43,12 +43,12 @@ public class VictimaService extends BaseService {
                 command.closeHandlerSession();
         }
 
-        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "El ID " + victimaId + " del historico ha sido encontrado correctamente")).build();
+        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "El ID " + victimaId + " de la victima ha sido encontrado correctamente")).build();
     }
 
     @GET
     @Path("/todos")
-    public Response getAllHistorico() {
+    public Response getAllVictima() {
         List <Usuario_VictimaDto> responseDTO = null;
         GetAllVictimaCommand command = null;
 
@@ -62,17 +62,17 @@ public class VictimaService extends BaseService {
             }
 
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>(e, "Error interno al ejecutar la ruta todos: " + e.getMessage())).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>(e, "Error interno al ejecutar la ruta todas las victimas: " + e.getMessage())).build();
         } finally {
             if (command != null)
                 command.closeHandlerSession();
         }
 
-        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "Todos los historicos se han obtenido correctamente")).build();
+        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "Todos las victimas se han obtenido correctamente")).build();
     }
 
     @POST
-    public Response addHistorico(Usuario_VictimaDto historicoUsuarioDto) {
+    public Response addVictima(Usuario_VictimaDto historicoUsuarioDto) {
         Usuario_Victima entity;
         Usuario_VictimaDto responseDTO = null;
         CreateVictimaCommand command = null;
@@ -83,62 +83,62 @@ public class VictimaService extends BaseService {
             command.execute();
             responseDTO = Usuario_VictimaMapper.mapEntityToDto(command.getReturnParam());
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al momento de registrar un historico", e.getMessage())).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al momento de registrar una victima", e.getMessage())).build();
         } finally {
             if (command != null)
                 command.closeHandlerSession();
         }
 
-        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "El historico ha sido creado correctamente")).build();
+        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "La victima ha sido creado correctamente")).build();
     }
 
     @DELETE
     @Path("/{id}")
-    public Response deleteHistorico(@PathParam("id") long userId) {
+    public Response deleteVictima(@PathParam("id") long victimaId) {
         Usuario_Victima entity;
         Usuario_VictimaDto responseDTO = null;
         DeleteVictimaCommand command = null;
 
         try {
-            entity = Usuario_VictimaMapper.mapDtoToEntity(userId);
+            entity = Usuario_VictimaMapper.mapDtoToEntity(victimaId);
             command = CommandFactory.createDeleteUsuario_VictimaCommand(entity);
             command.execute();
 
             if (command.getReturnParam() != null)
                 responseDTO = Usuario_VictimaMapper.mapEntityToDto(command.getReturnParam());
             else
-                return Response.status(Response.Status.OK).entity(new CustomResponse<>("No se pudo eliminar el historico con ese ID")).build();
+                return Response.status(Response.Status.OK).entity(new CustomResponse<>("No se pudo eliminar la victima con ese ID")).build();
 
 
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al momento de crear un usuario", e.getMessage())).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al momento de eliminar una victima", e.getMessage())).build();
         } finally {
             if (command != null)
                 command.closeHandlerSession();
         }
 
-        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "La historico ha sido eliminado correctamente")).build();
+        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "La victima ha sido eliminado correctamente")).build();
     }
 
     @PUT
-    public Response updateHistorico(Usuario_VictimaDto historicoUsuarioDto) {
+    public Response updateHistorico(Usuario_VictimaDto usuarioVictimaDto) {
         Usuario_Victima entity;
         Usuario_VictimaDto responseDTO = null;
         UpdateVictimaCommand command = null;
         try {
-            entity = Usuario_VictimaMapper.mapDtoToEntity(historicoUsuarioDto);
+            entity = Usuario_VictimaMapper.mapDtoToEntity(usuarioVictimaDto);
             command = CommandFactory.createUpdateUsuario_VictimaCommand(entity);
             command.execute();
             if (command.getReturnParam() != null)
                 responseDTO = Usuario_VictimaMapper.mapEntityToDto(command.getReturnParam());
             else
-                return Response.status(Response.Status.OK).entity(new CustomResponse<>("No se pudo editar el ID: " + historicoUsuarioDto.getId()) + " debido a que no existe en la base de datos").build();
+                return Response.status(Response.Status.OK).entity(new CustomResponse<>("No se pudo editar el ID: " + usuarioVictimaDto.getId()) + " debido a que no existe en la base de datos").build();
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al actualizar la coordenada: " + e.getMessage())).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new CustomResponse<>("Error interno al actualizar la victima: " + e.getMessage())).build();
         } finally {
             if (command != null)
                 command.closeHandlerSession();
         }
-        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "El historico con el ID " + historicoUsuarioDto.getId() + " se actualizo correctamente")).build();
+        return Response.status(Response.Status.OK).entity(new CustomResponse<>(responseDTO, "La victima con el ID " + usuarioVictimaDto.getId() + " se actualizo correctamente")).build();
     }
 }
