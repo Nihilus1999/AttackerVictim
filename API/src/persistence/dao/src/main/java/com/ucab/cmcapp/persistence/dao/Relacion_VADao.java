a27117f8b2603f4a29de06608a750218ab91c609
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-12-20 20:48:29 -0400

    cambios menores

--- API/src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/dao/Relacion_VADao.java
+++ API/src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/dao/Relacion_VADao.java
@@ -35,20 +35,19 @@ public class Relacion_VADao extends BaseDao<Relacion_VA> {
     public List<Historico_Usuario> getPosicionByRelacionId(Relacion_VA relacionId) {
         List<Historico_Usuario> results;
         try {
-            //CriteriaQuery<Incident> query = _builder.createQuery(Incident.class);
-            //Root<Incident> root = query.from(Incident.class);
 
             String customSql = "SELECT hu.* FROM historico_usuario hu INNER JOIN ((SELECT MAX(hu.id) AS ultimo_registro, 'usuario_victima' AS entidad FROM historico_usuario hu INNER JOIN usuario u ON hu.id_usuario = u.id INNER JOIN usuario_victima uv ON u.id = uv.id_usuario INNER JOIN relacion_VA rva ON uv.id = rva.id_usuario_victima WHERE rva.id = 1 GROUP BY entidad) UNION (SELECT MAX(hu.id) AS ultimo_registro, 'usuario_atacante' AS entidad FROM historico_usuario hu INNER JOIN usuario u ON hu.id_usuario = u.id INNER JOIN usuario_atacante ua ON u.id = ua.id_usuario INNER JOIN relacion_VA rva ON ua.id = rva.id_usuario_atacante WHERE rva.id = 1 GROUP BY entidad)) ult ON hu.id = ult.ultimo_registro;\n";
             Query query = _em.createNativeQuery(customSql, Historico_Usuario.class);
 
             results = query.getResultList();
 
-            if (results.size() < 2) // Retornar null en lugar de []
+            if (results.size() < 2)
                 return null;
 
         } catch (NoResultException e) {
-            //return Collections.emptyList();  // En caso de que quieras retornar []
+
             return null;
+            
         } catch (Exception e) {
             throw new CupraException(e.getMessage());
         }
