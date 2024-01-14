1f51d61fa6747fcce7c40a43c319102882771d0d
Jose El Asmar <jose-elasmar1999@outlook.com>
2024-01-13 22:43:23 -0400

    logger y javadoc del zonaSegura DAO

--- API/src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/dao/Zona_SeguraDao.java
+++ API/src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/dao/Zona_SeguraDao.java
@@ -3,6 +3,7 @@ package com.ucab.cmcapp.persistence.dao;
 import com.ucab.cmcapp.common.entities.Usuario_Victima;
 import com.ucab.cmcapp.common.entities.Zona_Segura;
 import com.ucab.cmcapp.common.exceptions.CupraException;
+import com.ucab.cmcapp.common.exceptions.NotFoundException;
 import com.ucab.cmcapp.persistence.DBHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,8 +33,15 @@ public class Zona_SeguraDao extends BaseDao<Zona_Segura> {
         _builder = _em.getCriteriaBuilder();
     }
 
+    /**
+     * Devuelve una lista de zonas seguras apartir del id de usuario_victima
+     * @param victimaId variable entera del id de victima
+     * @return devuelve una lista de zonas seguras
+     */
+
     public List<Zona_Segura> getZonaByVictimaId(Usuario_Victima victimaId) {
         List<Zona_Segura> results;
+        _logger.debug(String.format("tomando de Zona_SeguraDao.getZonaByvictimaId: parametro {%s}", victimaId));
         try {
             CriteriaQuery<Zona_Segura> query = _builder.createQuery(Zona_Segura.class);
             Root<Zona_Segura> root = query.from(Zona_Segura.class);
@@ -47,12 +55,13 @@ public class Zona_SeguraDao extends BaseDao<Zona_Segura> {
                 return null;
 
         } catch (NoResultException e) {
-            //return Collections.emptyList();  // En caso de que quieras retornar []
-            return null;
+            _logger.error( String.format( "Error Zona_SeguraDao.getZonaByVictimaId: No Result {%s}", e.getMessage() ) );
+            throw new NotFoundException("Zona Segura del id victima no existe");
         } catch (Exception e) {
+            _logger.error( String.format( "Error Zona_SeguraDao.getZonaByVictimaId: No Result {%s}", e.getMessage() ) );
             throw new CupraException(e.getMessage());
         }
-
+        _logger.debug(String.format("Dejando Zona_SeguraDao.getZonaByVictimaId: result {%s}", results));
         return results;
     }
 }
